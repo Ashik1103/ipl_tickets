5da2407284ccfb593877b5d413d6c742bf59690c
Ashik <haja.ashik.sg@gretail.com>
2025-04-16 00:34:35 +0530

    feat: use env

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from selenium.webdriver.common.by import By
 import firebase_admin
 from firebase_admin import credentials, messaging
 from flask import Flask
-
+import json
+import os
 app = Flask(__name__)
 
 
-cred = credentials.Certificate("service_account.json")
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
-
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+firebase_dict = json.loads(firebase_json)
+cred = credentials.Certificate(firebase_dict)
 def send_push_notification(token, title, body):
     message = messaging.Message(
         notification=messaging.Notification(
